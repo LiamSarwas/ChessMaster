06cd992c318d87cc6057095843316011338566a1
Regan Sarwas <rsarwas@gmail.com>
2016-10-17 12:28:04 -0800

    first try at pgn parser

--- chess/main.swift
+++ chess/main.swift
@@ -6,6 +6,22 @@
 //  Copyright © 2015 Regan Sarwas. Alocation rights reserved.
 //
 
+let movetext = "1. e4 e5 2. Nf3 Nc6 3. Bb5 a6 4. Ba4 Nf6 5. O-O Be7 6. Re1 b5 7. Bb3 d6 8. c3\n" +
+"O-O 9. h3 Nb8 10. d4 Nbd7 11. c4 c6 12. cxb5 axb5 13. Nc3 Bb7 14. Bg5 b4 15.\n" +
+"Nb1 h6 16. Bh4 c5 17. dxe5 Nxe4 18. Bxe7 Qxe7 19. exd6 Qf6 20. Nbd2 Nxd6 21.\n" +
+"Nc4 Nxc4 22. Bxc4 Nb6 23. Ne5 Rae8 24. Bxf7+ Rxf7 25. Nxf7 Rxe1+ 26. Qxe1 Kxf7\n" +
+"27. Qe3 Qg5 28. Qxg5 hxg5 29. b3 Ke6 30. a3 Kd6 31. axb4 cxb4 32. Ra5 Nd5 33.\n" +
+"f3 Bc8 34. Kf2 Bf5 35. Ra7 g6 36. Ra6+ Kc5 37. Ke1 Nf4 38. g3 Nxh3 39. Kd2 Kb5\n" +
+"40. Rd6 Kc5 41. Ra6 Nf2 42. g4 Bd3 43. Re6 1/2-1/2"
+
+if let history = try PGNDeserializer.parseMoveText(movetext) {
+    print(history)
+} else {
+    print("parser error")
+}
+
+
+
 print("Check modifying board does not change game")
 let fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
 if let game = Game(fromFEN: fen) {
